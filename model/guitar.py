4cc2a75fd020ecf2a8807751976af052a847eae8
keremkoseoglu <kerem@keremkoseoglu.com>
2020-07-06 12:16:29 +0300

    dont select the same instrument subsequently Fixes #50

--- model/guitar.py
+++ model/guitar.py
@@ -1,15 +1,20 @@
 """ Guitar module """
 import random
-from config import get_configuration
+from config import get_configuration, get_storage, save_storage
 
 
 def get_random_guitar() -> dict:
     """ Returns a random guitar """
     config = get_configuration()
-    random_number = random.randint(1, 100)
+    storage = get_storage()
 
-    for guitar in config["instruments"]:
-        if guitar["from"] <= random_number and guitar["to"] >= random_number:
-            return guitar
+    while True:
+        random_number = random.randint(1, 100)
+        for guitar in config["instruments"]:
+            if guitar["from"] <= random_number and guitar["to"] >= random_number:
+                if guitar["type"] != storage["last_instrument"]:
+                    storage["last_instrument"] = guitar["type"]
+                    save_storage(storage)
+                    return guitar
 
     raise Exception("Couldn't select an instrument")
