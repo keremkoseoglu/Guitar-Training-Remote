0d372098c6a6030c003401df30a06d9259051fb7
Dr. Kerem Koseoglu <kerem@keremkoseoglu.com>
2025-07-26 18:15:46 +0300

    fixes #224

--- test/test_quit_metronome.py
+++
@@ -1,9 +0,0 @@
-""" Test methods """
-import subprocess
-from model.exercise_helper import get_external_metronome_helper, quit_metronome
-
-def test_quit_metronome():
-    """ Test quit metronome """
-    metro_helper = get_external_metronome_helper(100)
-    subprocess.Popen(metro_helper.params["command"], shell=True)
-    quit_metronome()
